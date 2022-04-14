24ddd5ad1adcd5d4e4eb44ee8759cf987933fee2
Kand1 <vlad.volodin.01@mail.ru>
2022-04-14 20:58:27 +0300

    modify deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+
+git push -f https://github.com/Kand1/vue2-friend-list-test-case.git master:gh-pages
+
+cd -
